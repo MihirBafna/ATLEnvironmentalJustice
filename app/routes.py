fe5fe32d05ba0811ed325a42fa97d748964c07f6
brettpietruszka <brettpie.08@gmail.com>
2020-10-21 03:41:15 +0000

    implemented nginx

---
+++ app/routes.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template, redirect, request
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/solutions')
+def solutions():
+    return render_template('solutions.html')
